6f36bc1dab1f6768f4b662e263962d3e3bec98c1
Jordan Knapp <jordan@calm.com>
2015-10-15 16:19:17 -0700

    Initial Commit

---
+++ MontageApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MontageApp
+//
+//  Created by Jordan Knapp on 10/15/15.
+//  Copyright © 2015 Montage App, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MontageApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MontageApp
+//
+//  Created by Jordan Knapp on 10/15/15.
+//  Copyright © 2015 Montage App, LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
